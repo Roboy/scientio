dd16ad3beb9ec7f31e0d971d39abc49d9d3ced07
Negin <neginikarimi@gmail.com>
2018-11-08 23:43:05 +0100

    added further methods

--- node.py
+++ node.py
@@ -21,7 +21,6 @@ class NodeModel(object):
         return self.labels
 
     def set_label(self, label):
-        #chech if labels == null
         if self.labels is None:
             self.labels = set()
         self.label = label
@@ -57,7 +56,8 @@ class NodeModel(object):
         if self.properties is None:
             self.properties = dict()
         if key is not None and property is not None:
-            self.properties.update(key, property_)
+            new_entry = {key, property_}
+            self.properties.update(new_entry)
 
     def get_relationships(self):
         return None if (self.relationships is None or len(self.relationships) == 0) else self.relationships
@@ -76,5 +76,55 @@ class NodeModel(object):
     def set_relationship(self, key, ids):
         if self.relationships is None:
             self.relationships = dict()
+
         if key is not None and ids is not None:
-            # here i am its not a todo
+            if key in self.relationships:
+                self.relationships.get(key).append(ids)
+            else:
+                new_entry = {key, ids}
+                self.relationships.update(new_entry)
+
+    def has_relationship(self, relationship):
+        return not (self.get_relationship(relationship) is None) and not self.get_relationship(relationship)
+
+    # TODO defne the neo4j properties
+    # def set(self, node: NodeModel):
+    #    if node is not None:
+    #        self.set_id(node.get_id())
+    #        self.set_relationships()
+
+    # def get_name(self):
+
+    # def add_name():
+
+    # def checkRelationshipAvailability()
+
+    # def getPurityRelationships()
+
+    # def isLegal()
+
+    def reset_id(self):
+        self.id = 0
+
+    def reset_label(self):
+        self.label = None
+
+    def reset_labels(self):
+        self.labels = set()
+
+    def reset_properties(self):
+        self.properties = dict()
+
+    def reset_relationships(self):
+        self.relationships = dict()
+
+    def reset_node(self):
+        self.reset_id()
+        self.reset_label()
+        self.reset_labels()
+        self.reset_properties()
+        self.reset_relationships()
+
+    # @ Override : equals(Object obj)
+    # @ Override : hashCode()
+    # @ Override : toString()
