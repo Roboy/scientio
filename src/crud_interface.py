6f3f3e48df51d30c575752cf8530016e1feeaff7
Negin <neginikarimi@gmail.com>
2018-11-15 22:47:26 +0100

    teeny-tiny refactoring

--- src/crud_interface.py
+++ src/crud_interface.py
@@ -8,16 +8,16 @@ class CRUD(object):
     # Create a node
     @staticmethod
     def create(request: memory_node.MemoryNodeModel):
-        if not request.get_label().__eq__(neo4j_label.Neo4jLabel.other) and \
-                not request.get_label().__eq__(neo4j_label.Neo4jLabel.none):
-            return neo4j.Neo4j.create_node(request)
+        if not request.get_label().__eq__(neo4j_label.Neo4jLabel().other) and \
+                not request.get_label().__eq__(neo4j_label.Neo4jLabel().none):
+            return neo4j.Neo4j().create_node(request)
         return None
 
     # Get node by ID
     @staticmethod
     def retrieve(id_, memory: memory_interface.MemoryInterface):
         if id_ > 0:
-            return neo4j.Neo4j.get_node_by_id(id_, memory)
+            return neo4j.Neo4j().get_node_by_id(id_, memory)
         elif id_ == 0:
             pass
         # TODO what labels? We do not have a node do we?
@@ -27,9 +27,9 @@ class CRUD(object):
     # Update Nodes
     @staticmethod
     def update(request: memory_node.MemoryNodeModel):
-        if not request.get_label().__eq__(neo4j_label.Neo4jLabel.other) and \
-                not request.get_label().__eq__(neo4j_label.Neo4jLabel.none):
-            return neo4j.Neo4j.update_node(request)
+        if not request.get_label().__eq__(neo4j_label.Neo4jLabel().other) and \
+                not request.get_label().__eq__(neo4j_label.Neo4jLabel().none):
+            return neo4j.Neo4j().update_node(request)
         return False
 
     # Delete a Node
@@ -37,5 +37,5 @@ class CRUD(object):
     def delete(request: memory_node.MemoryNodeModel):
         if request.get_id() > 0:
             # TODO: if id != 0 and retrieve(argument) == null returns true else returns false
-            return neo4j.Neo4j.delete(request)
+            return neo4j.Neo4j().delete(request)
         return False
