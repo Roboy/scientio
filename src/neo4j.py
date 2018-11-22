e349e6319e5123a5922855f27d873d29b6235920
Wagram Airiian <vagram.airiian@tum.de>
2018-11-22 16:28:51 +0100

    Added an implementation stub for query execution wrapper

--- src/neo4j.py
+++ src/neo4j.py
@@ -1,22 +1,74 @@
-# I'm a stub for the neo4j connection
-from src import memory_node_model
-from src import memory_interface
+from typing import Optional
+
+from neo4j import GraphDatabase
+
+from src.memory_node_model import MemoryNodeModel
 
 
-# Probably Neo4j methods can be all static ...
 class Neo4j(object):
-    def create_node(self, node: memory_node_model.MemoryNodeModel):
-        print(node.__str__())
-        return None
+    _driver: GraphDatabase.driver = None
 
-    def get_node(self, node: memory_node_model.MemoryNodeModel, memory: memory_interface.MemoryInterface):
-        pass
+    def __init__(self, **kwargs):
+        if kwargs is not None:
+            if kwargs['neo4j_address']:
+                uri = kwargs['neo4j_address']
+            else:
+                raise KeyError
+            if kwargs['neo4j_username']:
+                user = kwargs['neo4j_username']
+            else:
+                raise KeyError
+            if kwargs['neo4j_password']:
+                password = kwargs['neo4j_password']
+            else:
+                raise KeyError
+        else:
+            raise ValueError
 
-    def get_node_by_id(self, id_, memory: memory_interface.MemoryInterface):
-        pass
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def update_node(self, node: memory_node_model.MemoryNodeModel):
-        pass
+    def close(self):
+        self._driver.close()
 
-    def delete(self, node: memory_node_model.MemoryNodeModel):
-        pass
+    @staticmethod
+    def _create_and_return_query(tx, query: str):
+        result = tx.run(query)
+        return result.single()[0]
+
+    def create_node(self, node: MemoryNodeModel) -> Optional[MemoryNodeModel]:
+        query = "" # builder from MNM
+        with self._driver.session() as session:
+            result = session.write_transaction(self._create_and_return_query, query=query)
+        result_node = None # node from result
+        return result_node
+
+    def get_node(self, node: MemoryNodeModel) -> Optional[MemoryNodeModel]:
+        query = ""  # builder from MNM
+        with self._driver.session() as session:
+            result = session.write_transaction(self._create_and_return_query, query=query)
+        result_node = None  # node from result
+        return result_node
+
+    def get_node_by_id(self, node_id: int) -> Optional[MemoryNodeModel]:
+        query = ""  # builder from MNM
+        with self._driver.session() as session:
+            result = session.write_transaction(self._create_and_return_query, query=query)
+        result_node = None  # node from result
+        return result_node
+
+    def update_node(self, node: MemoryNodeModel) -> Optional[MemoryNodeModel]:
+        query = ""  # builder from MNM
+        with self._driver.session() as session:
+            result = session.write_transaction(self._create_and_return_query, query=query)
+        result_node = None  # node from result
+        return result_node
+
+    def delete_node(self, node: MemoryNodeModel) -> bool:
+        query = ""  # builder from MNM
+        with self._driver.session() as session:
+            result = session.write_transaction(self._create_and_return_query, query=query)
+
+        if self.get_node_by_id(node.get_id()) is None:
+            return True
+        else:
+            return False
